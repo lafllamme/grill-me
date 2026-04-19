37105cbdc9b6aa0b6611f2e5491bd10988f2f921
Lafllamme <teke900@gmail.com>
2026-04-19 17:25:37 +0200

    feat: refactor option to setup store

--- app/stores/roastStore.ts
+++ app/stores/roastStore.ts
@@ -1,19 +1,22 @@
-export const useRoastStore = defineStore('roastStore', {
-  state: () => ({
-    githubUsername: '',
-  }),
-  getters: {
-    trimmedUsername: state => state.githubUsername.trim(),
-    canSubmit(): boolean {
-      return this.trimmedUsername.length > 0
-    },
-  },
-  actions: {
-    setUsername(value: string) {
-      this.githubUsername = value
-    },
-    clearUsername() {
-      this.githubUsername = ''
-    },
-  },
+export const useRoastStore = defineStore('roastStore', () => {
+  const githubUsername = ref('')
+
+  const trimmedUsername = computed(() => githubUsername.value.trim())
+  const canSubmit = computed(() => trimmedUsername.value.length > 0)
+
+  const setUsername = (value: string) => {
+    githubUsername.value = value
+  }
+
+  const clearUsername = () => {
+    githubUsername.value = ''
+  }
+
+  return {
+    githubUsername,
+    trimmedUsername,
+    canSubmit,
+    setUsername,
+    clearUsername,
+  }
 })
